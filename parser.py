f09a723c032cd8becf36f737dce616840483607c
Antoine Mureddu <antoine.mureddu@softathome.com>
2021-05-04 18:41:29 +0200

    generate teams (not random)

--- parser.py
+++ parser.py
@@ -1,6 +1,8 @@
+import json
 import os
 import pandas
-from utils import create_empty_dict, store_infos, get_sport_config
+from utils import create_empty_dict, store_infos, get_sport_config, get_athletes
+from utils import config_has_player_per_team_limit, config_has_team_limit, generate_teams
 
 excel_sheet = pandas.read_excel(os.path.join(os.getcwd()+"/JO_2020.xlsx"), sheet_name=None, engine="openpyxl")
 athletes = create_empty_dict(excel_sheet)
@@ -9,7 +11,6 @@ useful_data = ("Nom Prénom",
                "Sexe")
 sports_name = ("10 km de Meyssiez (9,5 km 150 D+)",
           "Volley",
-          "Petanque/Molky",
           "Waterpolo (Tournoi par équipe de 5 )",
           "Lancer de tong",
           "Ping pong",
@@ -26,11 +27,17 @@ sports_name = ("10 km de Meyssiez (9,5 km 150 D+)",
           "Concours de pizza",
           "Concours de gros cuissots",
           "Maximum de distance au rameur en équipe")
+sports = dict()
+for sport_name in sports_name:
+    sports[sport_name] = dict()
 for sheet in excel_sheet:
     for column_name in excel_sheet[sheet]:
         if column_name in useful_data:
             athletes = store_infos(excel_sheet[sheet][column_name], athletes, column_name)
         if column_name in sports_name:
-            print(get_sport_config(column_name))
+            sport_config = get_sport_config(column_name)
+            sport_votes = excel_sheet[sheet][column_name]
+            sports[column_name]["athletes"] = get_athletes(sport_votes, athletes)
+            sports[column_name]["teams"] = generate_teams(sport_config, get_athletes(sport_votes, athletes))
     break
-print(athletes)
+json.dump(sports["Volley"], open("test.json", "w"), ensure_ascii=False)
--- utils.py
+++ utils.py
@@ -23,7 +23,68 @@ def get_sport_config(sport_name):
     file_name = get_file_name(sport_name)
     return json.load(open(f"configs/{file_name}"))
 
+
 def get_file_name(sport_name):
-    lut = json.load(open("configs/lut.json"))
+    lut = json.load(open("configs/LUT.json"))
     return lut[sport_name]
 
+
+def get_athletes(sport_votes, athletes):
+    athlete_id = 0
+    athletes_list = []
+    yes_list = ("Participant", "Coureur", "Cuisinier (tout seul ou en equipe)", "Chaud")
+    for vote in sport_votes:
+        if vote in yes_list:
+            athletes_list.append(athletes[str(athlete_id)])
+        athlete_id += 1
+    return athletes_list
+
+
+def config_has_team_limit(config):
+    return "Wanted teams" in config
+
+
+def config_has_player_per_team_limit(config):
+    return "Wanted players per team" in config
+
+
+def generate_teams(config, athletes):
+    number_of_athletes = len(athletes)
+    if config_has_player_per_team_limit(config):
+        number_of_teams = int(number_of_athletes / config["Wanted players per team"])
+        if number_of_athletes % config["Wanted players per team"]:
+            if "Accepted players per team" in config:
+                more_teams = config["Accepted players per team"] < config["Wanted players per team"]
+                number_of_teams = number_of_teams + 1 if more_teams else number_of_teams - 1
+    elif config_has_team_limit(config):
+        number_of_teams = int(number_of_athletes / config["Wanted teams"])
+    boobs_number = get_boobs_number(athletes)
+    teams = dict()
+    for team_number in range(number_of_teams):
+        teams[f'team_{team_number}'] = []
+    while boobs_number:
+        for team in teams:
+            for athlete in athletes:
+                if athlete["Sexe"] == "F":
+                    teams[team].append(athlete)
+                    athletes.remove(athlete)
+                    boobs_number -= 2
+                    break
+    for team in teams:
+        if len(teams[team]) < len(teams['team_0']):
+            teams[team].append(athletes[0])
+            athletes.remove(athletes[0])
+    while athletes:
+        for team in teams:
+            if athletes:
+                teams[team].append(athletes[0])
+                athletes.remove(athletes[0])
+    return teams
+
+
+def get_boobs_number(athletes):
+    boobs_number = 0
+    for athlete in athletes:
+        if athlete["Sexe"] == "F":
+            boobs_number += 2
+    return boobs_number
