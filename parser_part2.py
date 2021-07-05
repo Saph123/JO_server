26defee35703e1fe0165edd60a7dee08afa8ddc9
Antoine Mureddu <antoine.mureddu@softathome.com>
2021-07-05 19:40:17 +0200

    fix uniqueid

--- parser_part2.py
+++ parser_part2.py
@@ -27,17 +27,18 @@ for sport_name in sports_name:
     excel_sheet = pandas.read_excel(path, sheet_name=sport_name, engine="openpyxl")
     teams_list = dict()
     teams_list["Teams"] = []
+    unique_id = 1
     for column_name in excel_sheet: 
-        unique_id = 1
         if "team" in column_name:
             team_dict = dict()
             team_dict["stillingame"] = "True" 
             team_dict["uniqueid"] = unique_id
             team_dict["Players"] = concatenate_players(excel_sheet, column_name)
             teams_list["Teams"].append(team_dict)
+            unique_id += 1
         file_name = get_file_name(sport_name)
-        with open(f"teams/{file_name}", "w") as file:
-            json.dump(teams_list, file, ensure_ascii=False)
+    with open(f"teams/{file_name}", "w") as file:
+        json.dump(teams_list, file, ensure_ascii=False)
     sport_config = get_sport_config(sport_name)
     if sport_config["Table/Pool/Both/None"] == 0:
         print(sport_name)
