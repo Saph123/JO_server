5383c46da1ace6d43ebf77e76f7784387eb86825
saph123 <max.saph@gmail.com>
2021-08-17 16:27:00 +0200

    Update cluedo with the username who is asking

--- serve_JO.py
+++ serve_JO.py
@@ -4,14 +4,22 @@ import ssl
 import sys
 import os
 import socket
+
+from requests.api import head
 import mariadb
 import simplejson
 import time    
 import json
 import hashlib
+import requests
+import re
 from utils import fix_json, update_playoff_match, user_is_authorized, update_list, update_poules_match, log, fix_json
 from utils import update_global_results
+
+
 root_dir = os.path.dirname(os.path.realpath(__file__))
+
+first_time = True
 
 try:
     conn = mariadb.connect(
@@ -26,7 +34,7 @@ except mariadb.Error as e:
 cur = conn.cursor()
 conn.autocommit = True
 class myHandler (BaseHTTPRequestHandler):
-
+    
     #Handler for the GET requests
     def do_GET(self):
         print("get")
@@ -105,12 +113,67 @@ class myHandler (BaseHTTPRequestHandler):
             update_global_results()
             fix_json()
             log(sport, username, data)
+        elif "pushtoken" in self.path:
+            data = json.loads(post_data.decode('utf-8'))
+            print(data)
+            if data.get("token"):
+                if not os.path.exists("tokens.txt"):
+                    open("tokens.txt", "w")
+                if data.get("token") not in open("tokens.txt", "r").read(): # just to be sure we don't write again the same
+                    if data.get("username"):
+                        open("tokens.txt","a").write(data.get("token") + ":"+ data.get("username") +"\n")
+                    else:
+                        open("tokens.txt","a").write(data.get("token") + ":\n")
+                elif data.get("username") != "":
+                    lines = open("tokens.txt", "r").readlines()
+                    for local_token in lines:
+                        if data.get("token") in local_token:
+                            if data.get("username") != local_token.split(":")[-1].replace("\n", ""):
+                                print("This token changed login!")
+                                # lines[lines.index(local_token)] = data.get("token") + ":"+ data.get("username") +"\n"
+                                to_update = True
+                                break
+                            else:
+                                to_update = False
+                                print("I know already this token")
+                    if to_update:
+                        raw_txt = open("tokens.txt", "r").read()
+                        print("(" + data.get("token") + ".*)")
+                        regexp = re.findall("(" + re.escape(data.get("token")) + ".*)", raw_txt)[0]
+                        raw_txt.replace(regexp, data.get("token") + ":"+ data.get("username") +"\n")
+                        open("tokens.txt", "w").write(raw_txt)
+
+            self.send_response(200,"fdp")
+        elif "cluedo" in self.path:
+            username = json.loads(post_data.decode('utf-8')).get("cluedo")
+            print(username)
+            if os.path.exists("lasttimecluedo"):
+                last_time = float(open("lasttimecluedo", "r").read())
+            else:
+                last_time = time.time() - 6 * 60
+            if time.time() > (last_time + 5 * 60): # filter 5 mins
+                print("cluedotime")
+                tokens = open("tokens.txt", "r").readlines()
+                for token in tokens:
+                    data = {"to": token.split(":")[0].replace(":",""), "title":"CLUEDO!", "body":"Demandé par : %s" % username}
+                    req = requests.post("https://exp.host/--/api/v2/push/send", data = data)
+                    if re.findall("DeviceNotRegistered", req.text):
+                        print("device not registered anymore so removing the line")
+                        full_txt = open("tokens.txt", "r").read()
+                        open("tokens.txt", "w").write(full_txt.replace(token, ""))
+                open("lasttimecluedo", "w").write(str(time.time()))
+            else:
+                print("ignore as it's less than 5 mins since last")
+      
+
+
         return
 
-
+ 
 
 def is_port_in_use(ip, port):
     res = False
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.bind((ip, port))
@@ -122,8 +185,6 @@ def is_port_in_use(ip, port):
 
 def start_server():
     
-    
-
     try:
         print("Starting http server :")
         server_address = ('', 7070)
